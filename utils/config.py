18026f3809fdf627322d31c8149675b7b71f2a4c
cloud_user-at-590183885307 <cloud_user@test.com>
2025-08-01 15:57:09 +0530

    Basic setup

---
+++ utils/config.py
@@ -0,0 +1,9 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+
+_ = load_dotenv()
+
+class Settings(BaseSettings):
+    google_api_key: str
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
